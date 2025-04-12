f6d32c3eb5fc02da5b9dd2b890f5f20bd8e017a0
TahirHusayn <tahirhussain5112@gmail.com>
2025-04-12 15:08:40 +0530

    Fixed shadcn issue font issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  preload: true,
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  preload: true,
+});
 
 export const metadata: Metadata = {
   title: "MarkIt",
@@ -17,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${GeistSans.className} ${GeistMono.className} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         cz-shortcut-listen="true"
       >
         {children}
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -24,114 +24,47 @@ export function SignupForm({
     >
       <div className={cn("flex flex-col gap-6", className)} {...props}>
         <Card>
-          <CardHeader className="text-center">
-            <CardTitle className="text-xl">Hi</CardTitle>
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
             <CardDescription>Singup with your credentials</CardDescription>
           </CardHeader>
           <CardContent>
             <form>
-              <div className="grid gap-6">
-                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"></div>
-                <div className="grid gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Student ID</Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="22BC8010"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Name</Label>
-                    <Input
-                      id="name"
-                      type="name"
-                      placeholder="narendra modi"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="me@example.com"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Ph.</Label>
-                    <Input
-                      id="phone"
-                      type="phone"
-                      placeholder="+9177328xxxxx"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Father&aposs Name</Label>
-                    <Input
-                      id="name"
-                      type="name"
-                      placeholder="amit shah"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Mother&aposs Name</Label>
-                    <Input
-                      id="name"
-                      type="name"
-                      placeholder="nirmala sitaraman"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Father Ph.</Label>
-                    <Input
-                      id="phone"
-                      type="phone"
-                      placeholder="+94135xxxxx"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Mother&aposs Ph.</Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="+9175974xxxxx"
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <div className="flex items-center">
-                      <Label htmlFor="password">Password</Label>
-                    </div>
-                    <Input id="password" type="password" required />
-                  </div>
-
-                  <div className="grid gap-3">
-                    <Button type="submit" className="w-full">
-                      Sign Up
-                    </Button>
-                  </div>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Student ID</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
                 </div>
-                <div className="text-center text-sm">
-                  Already have an account?{" "}
-                  <a href="/login" className="underline underline-offset-4">
-                    Log In
-                  </a>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
                 </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
               </div>
             </form>
           </CardContent>
         </Card>
-        <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-          By clicking continue, you agree to our{" "}
-          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-        </div>
       </div>
     </FpjsProvider>
   );
